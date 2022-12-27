c3591ec7acc79efdb7b2b6f929085143ecefbf45
Marina Yezerska <118081961+MarinaYez@users.noreply.github.com>
2022-12-27 02:43:30 +0200

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,10 @@
+let userName = prompt("Enter your name", 'Name');
+alert(`Your name is: ${userName}`)
+let userSurname = prompt("Enter your surname", 'Surname');
+alert(`Your full name is: ${userName} ${userSurname}`)
+let age = prompt("How old are you?", 'Age');
+alert(`Your full name is: ${userName + userSurname} your age is: ${age}`)
+let admin = confirm("Are you admin?");
+alert(`Your full name is: ${userName} ${userSurname} ,your age is: ${age} admin status:${admin}`)
+alert('Please enter your data');
+
